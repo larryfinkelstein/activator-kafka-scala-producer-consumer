95da5637bd6085cd7f02fcddb6a44e222c6dd0d5
Larry Finkelstein <larry.finkelstein@bms.com>
2016-09-05 23:48:12 -0400

    Remove depratecated calls for producer and keyedmessage. Pass settings to producer implementation Add timestamp/benchmarking output

--- src/main/scala/com/knoldus/kafka/demo/ProducerApp.scala
+++ src/main/scala/com/knoldus/kafka/demo/ProducerApp.scala
@@ -1,20 +1,40 @@
 package com.knoldus.kafka.demo
 
-import com.knoldus.kafka.producer.{AsyncProducer, KafkaProducer}
+import com.knoldus.kafka.producer.{AsyncProducer, KafkaProducer, KafkaProducerNew}
+
+import scala.util.Random
 
 
 object ProducerApp extends App {
 
   val topic = "demo-topic"
 
-  val producer = new AsyncProducer("localhost:9092")
-  //val producer = new KafkaProducer("localhost:9092")
-
+  val msgSize: Int = (1024 * .25).toInt
   val batchSize = 100
+  val bufferMemory = 16 * 1024 * 1024
+  val numTransactions = 1000000
+  val compression = "none" // none, gzip, snappy, or lz4
+  val rString = Random.alphanumeric.take(msgSize).mkString
 
-  (1 to 1000000).toList.map(no => "Message " + no).grouped(batchSize).foreach { message =>
-    println("Sending message batch size " + message.length)
-    producer.send(topic, message)
+  //val producer = new AsyncProducer("kafka01:9092,kafka02:9092,kafka03:2092")
+  //val producer = new KafkaProducer("localhost:9092")
+  val producer = new KafkaProducerNew("kafka01:9092,kafka02:9092,kafka03:2092", batchSize, bufferMemory, compression)
+
+  val t0 = System.nanoTime()
+  (1 to numTransactions).toList.map(no => s"Msg # ${no} ${rString}")
+//    .grouped(batchSize)
+    .foreach { message =>
+    //    println("Sending message batch size " + message.length)
+//    print('.')
+//    println(message)
+      producer.send(topic, message)
   }
 
+  val t1 = System.nanoTime()
+  val secs = (t1 - t0) / 1E9
+  val t = (t1 - t0)
+  println
+  println(s"Sent a total of ${numTransactions} messages (size=${msgSize}, batch=${batchSize}, buffer=${bufferMemory}, compression=${compression}) in ${secs} seconds")
+  println(s"${numTransactions / secs} transactions per sec")
+  producer.close()
 }
